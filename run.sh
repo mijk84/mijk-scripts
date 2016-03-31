90b3558365f1f2b4799beee12da07c91018a9287
Mike Ladouceur <mgladouceur@gmail.com>
2016-03-31 06:38:51 -0400

    Create run.sh
    qemu run script for Linux

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#run.sh
+qemu="qemu-system-i386"
+kernel=
+initrd=
+cpu="core2duo"
+m="256"
+vga=
+soundhw=
+device=
+hda="hda.img"
+cdrom=
+fda=
+redir="tcp:8022::22"
+#Adding a space between brackets tricks the nographic setting into being used.
+nographic=" "
+boot="c"
+
+for n in kernel initrd cpu m vga soundhw hda cdrom fda device redir boot nographic
+do
+  if [ -n "${!n}" ]; then
+qemurun="$qemurun -$n ${!n}"
+  fi
+done
+
+
+$qemu $qemurun
