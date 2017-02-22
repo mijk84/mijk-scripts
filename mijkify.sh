eded07ed6508f0cec490d56fcb62736751691b2f
Mike Ladouceur <mgladouceur@gmail.com>
2017-02-22 08:34:27 -0500

    Update mijkify.sh

--- mijkify.sh
+++ mijkify.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+release=$(lsb_release -cs)
 wget -q -O- http://archive.getdeb.net/getdeb-archive.key | apt-key add -
 wget -O- http://archive.getdeb.net/getdeb-archive.key | apt-key add -
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4C9D234C
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F9CB8DB0
 
@@ -64,10 +66,14 @@ deb-src http://ppa.launchpad.net/nilarimogard/webupd8/ubuntu trusty main
 ## Run this command:  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F9CB8DB0
 deb-src http://ppa.launchpad.net/ubuntu-wine/ppa/ubuntu trusty main
 EOF
+
 #### WebUpd8 Java
 add-apt-repository -y ppa:webupd8team/java
+
+## Google Chrome
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
 apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
-apt-get install -y xserver-xorg xinit lightdm mesa-utils-extra mesa-vdpau-drivers libgl1-mesa-dri libgl1-mesa-glx build-essentials cmake qt5-qmake flex bison git subversion mercurial
+apt-get install -y xserver-xorg xinit lightdm mesa-utils-extra mesa-vdpau-drivers libgl1-mesa-dri libgl1-mesa-glx build-essentials cmake qt5-qmake flex bison git subversion mercurial oracle-java8-installer google-chrome-stable
