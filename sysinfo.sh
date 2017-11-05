aa77758a37d160d9fbd8de783950dfcf107927dc
Mike Ladouceur <mgladouceur@gmail.com>
2017-11-04 23:40:15 -0400

    Update sysinfo.sh
    CPU info shows number of cores now.

--- sysinfo.sh
+++ sysinfo.sh
@@ -3,7 +3,7 @@
 echo This simply copies your system information to the clipboard to paste in a chat room.
 echo
 
-SI="echo Hostname: $(hostname) \** OS: $(cat /etc/lsb-release | grep DESCRIPTION | cut -c 22- | head -c -2) \** CPU: $(cat /proc/cpuinfo | grep model | grep name | cut -c 14-) \** RAM: $(echo $(grep MemTotal /proc/meminfo | awk '{print $2}') / 1000000 | bc)GB \** VGA: $(lspci | grep VGA | awk '{$1=$2=$3=$4=""; print $0}') \** Uptime: $(uptime --pretty | cut -c 4-)"
+SI="echo Hostname: $(hostname) \** OS: $(cat /etc/lsb-release | grep DESCRIPTION | cut -c 22- | head -c -2) \** CPU: $(cat /proc/cpuinfo | grep cores | tail -n1 | awk '{ print $4 }')x $(cat /proc/cpuinfo | grep model | tail -n1 | awk '{$1=$2=$3=""; print $0}') \** RAM: $(echo $(grep MemTotal /proc/meminfo | awk '{print $2}') / 1000000 | bc)GB \** VGA: $(lspci | grep VGA | awk '{$1=$2=$3=$4=""; print $0}') \** Uptime: $(uptime --pretty | cut -c 4-)"
 
 if ! which xclip > /dev/null; then
     echo
@@ -14,7 +14,7 @@ fi
 
 if ! which bc > /dev/null; then
     echo
-    echo 'You'll need bc for this to work.'
+    echo You'll need bc for this to work.
     echo
     exit 1
 fi
@@ -28,6 +28,6 @@ case "$1" in
     -s) echo $($SI) ;;
     --shell) echo $($SI) ;;
     -?) echo Usage: sysinfo && echo      sysinfo -s [This pastes to the shell instead] && echo      system --shell [This pastes to the shell instead] ;;
-    --help) echo Usage: sysinfo && echo      sysinfo -s [This pastes to the shell instead] && echo      system --shell [This pastes to the shell instead] ;;
+    --help) echo Usage: sysinfo && echo      sysinfo -s [This pastes to the shell instead] && echo      sysinfo --shell [This pastes to the shell instead] ;;
     *) echo Try running this on its own or run: sysinfo --help ;;
 esac
