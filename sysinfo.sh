be7951df44dbe3d43c39332cc288c6216e0cb39b
Mike Ladouceur <mgladouceur@gmail.com>
2018-05-30 10:33:59 -0400

    Update sysinfo.sh
    Pretty like Github wants it.

--- sysinfo.sh
+++ sysinfo.sh
@@ -6,7 +6,12 @@ n=$(tput sgr0)
 echo This simply copies your system information to the clipboard to paste in a chat room.
 echo
 
-SI="echo ${b}Hostname${n}: $(hostname) ${b}• OS${n}: $(cat /etc/lsb-release | grep DESCRIPTION | cut -c 22- | head -c -2) ${b}• CPU${n}: $(cat /proc/cpuinfo | grep cores | tail -n1 | awk '{ print $4 }')x $(cat /proc/cpuinfo | grep model | tail -n1 | awk '{$1=$2=$3=""; print $0}') ${b}• RAM${n}: $(echo $(grep MemTotal /proc/meminfo | awk '{print $2}') / 1000000 | bc)GB ${b}• Storage${n}: $(df | grep '^/dev/[hs]d' | awk '{s+=$2} END {print s/1048576}')GB Total ($(df | grep '^/dev/[hs]d' | awk '{s+=$4} END {print s/1048576}')GB Free) ${b}• VGA${n}: $(lspci | grep VGA | awk '{$1=$2=$3=$4=""; print $0}') ${b}• Uptime${n}: $(uptime --pretty | cut -c 4-)"
+SI="echo ${b}Hostname${n}: $(hostname) \
+    ${b}• OS${n}: $(cat /etc/lsb-release | grep DESCRIPTION | cut -c 22- | head -c -2) \
+    ${b}• CPU${n}: $(cat /proc/cpuinfo | grep cores | tail -n1 | awk '{ print $4 }')x $(cat /proc/cpuinfo | grep model | tail -n1 | awk '{$1=$2=$3=""; print $0}') \
+    ${b}• RAM${n}: $(echo $(grep MemTotal /proc/meminfo | awk '{print $2}') / 1000000 | bc)GB \
+    ${b}• Storage${n}: $(df | grep '^/dev/[hs]d' | awk '{s+=$2} END {print s/1048576}')GB Total ($(df | grep '^/dev/[hs]d' | awk '{s+=$4} END {print s/1048576}')GB Free) \
+    ${b}• VGA${n}: $(lspci | grep VGA | awk '{$1=$2=$3=$4=""; print $0}') ${b}• Uptime${n}: $(uptime --pretty | cut -c 4-)" \
 
 if ! which xclip > /dev/null; then
     echo
