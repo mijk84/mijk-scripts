c7fa16851c38a3e1b7800b6e7d15c635d072e06a
Mike Ladouceur <mgladouceur@gmail.com>
2020-06-14 10:17:58 -0400

    Update to pantheonize.sh
    Removed the mpstark ppa because it doesn't even exist anymore. Removed stuff about desktop icons and changing wallpaper because it's no longer an issue. Download size probably different now. Not sure about lsb_release file change anymore or if it even makes a difference.

--- pantheonize.sh
+++ pantheonize.sh
@@ -6,7 +6,7 @@ fi
 
 cp /etc/lsb_release /etc/lsb_release.bak
 
-echo Pantheonize v0.0.1 by mijk
+echo Pantheonize v0.0.2 by mijk
 echo.
 echo This script will add the necessary PPA's to install the Pantheon window manager.
 echo You must ensure to have install X and a display manager beforehand.
@@ -17,7 +17,6 @@ echo.
         add-apt-repository -y ppa:elementary-os/daily
         add-apt-repository -y ppa:elementary-os/os-patches
         add-apt-repository -y ppa:elementary-os/testing
-        add-apt-repository -y ppa:mpstark/elementary-tweaks-daily
 
                 # Upgrade the OS because there are likely updates to your installation on this PPA.
                 # This will download about 250MB on average.
@@ -27,12 +26,6 @@ echo.
 
         # Time to install the packages, a good 750MB download here alone.
         apt-get install -y elementary-theme elementary-icon-theme elementary-default-settings elementary-desktop gtk2-engines-pixbuf gnome-themes-standard libcanberra-gtk3-module:i386 libcanberra-gtk3-module
-        apt-get install -y --no-install-recommends dconf-editor nautilus
-
-        # For some reason, there are no desktop icons, no method of right-clicking on the desktop
-        # and no way of changing the wallpaper exists. This enables these features.
-        su - mike -c gsettings set org.gnome.desktop.background show-desktop-icons true
-        su - mike -c gsettings set org.pantheon.desktop.cerbere monitored-processes "['wingpanel', 'plank', 'slingshot-launcher --silent', 'nautilus -n']"
 
          rm /etc/lsb_release
          mv /etc/lsb_release.bak /etc/lsb_release
