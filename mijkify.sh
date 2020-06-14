a800bb8d993e25501d5d01c5bcffdc964405d8db
Mike Ladouceur <mgladouceur@gmail.com>
2020-06-14 10:12:11 -0400

    Long overdo update on imjkify.sh
    Added a case block for gui or cli optiosn. Removed crap I don't need/use anymore. Pending removing some keys as I don't remember which are which. Added packages autotools-dev and autoconf, which I always end up needing at one point or another.

--- mijkify.sh
+++ mijkify.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
-release=$(lsb_release -cs)
+cli=build-essential cmake qt5-qmake flex bison git subversion mercurial autotools-dev autoconf google-chrome-stable
+gui=google-chrome-stable steam quassel-client transgui
+
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+
+case "$1" in
+'cli')
+apt-get install -y $cli
+;;
+'gui')
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4C9D234C
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F9CB8DB0
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B05498B7
-
-#### WebUpd8 Java
-yes | add-apt-repository -y ppa:webupd8team/java
 
 ## Google Chrome
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
 ## Steam
 sh -c 'echo "deb http://repo.steampowered.com/steam/ precise steam" >> /etc/apt/sources.list.d/steam.list'
-
-## Minecraft
-yes | add-apt-repository ppa:minecraft-installer-peeps/minecraft-installer
-
-apt-get update
-apt-get upgrade -y
-apt-get dist-upgrade -y
-apt-get install -y build-essential cmake qt5-qmake flex bison git subversion mercurial oracle-java8-installer google-chrome-stable steam minecraft-installer quassel-client transgui
+apt-get install -y $cli $gui
+;;
+'')
+echo "Usage: $0 [cli|gui]"
+;;
+esac
