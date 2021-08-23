62f4477c97360477a02c1187a87f43c106ef608a
Mike Ladouceur <mgladouceur@gmail.com>
2021-08-23 09:51:19 -0400

    Update qemu-update.cmd
    Made some legible edits and created the updfile variable so that it's user editable and you don't have to go digging for it in the code.

--- qemu-update.cmd
+++ qemu-update.cmd
@@ -1,9 +1,14 @@
 @echo off
+:: User editable variables
 set curl=C:\windows\system32\curl.exe
 set qemuurl=https://qemu.weilnetz.de/w64/
-set token=for /f "tokens=
-set qemuvar=delims==^<^>/ " %%n in ('%curl% -s https://qemu.weilnetz.de/w64/ ^| find ".exe"') do set
+set updfile=C:\qemu\update.ini
 
+:: Dev editable variables
+set token=for /f "tokens=
+set qemuvar=delims==^<^>/ " %%n in ('%curl% -s %qemuurl% ^| find ".exe"') do set
+
+:: The rest of the script
 echo Checking for updates ...
 echo.
 
@@ -15,4 +20,4 @@ echo.
 
 :: Check for update.
 for /f %%n in ('type C:\qemu\update.ini') do set lastupd=%%n
-	if %qemudt% GTR %lastupd% (%curl% -s %qemuurl%/%qemudl% -O && echo %qemudt%>C:\qemu\update.ini)  else echo No update yet.
+	if %qemudt% GTR %lastupd% (%curl% -s %qemuurl%/%qemudl% -O && echo %qemudt%>%updfile%)  else echo No update yet.
