ccd8c7977091b9a8afe9d201108912edb722d0a0
Mike Ladouceur <mgladouceur@gmail.com>
2022-01-14 16:08:55 -0500

    Update mijksrv-vm-w10.sh
    Added swtpm TPM 2.0 emulator, which runs as a daemon and then launches QEMu with the TPM "device".

--- mijksrv-vm-w10.sh
+++ mijksrv-vm-w10.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-qemu-system-x86_64 \
+## Emulated TPM 2.0 device
+mkdir /tmp/emulated_tpm
+swtpm socket --tpmstate dir=/tmp/emulated_tpm --ctrl type=unixio,path=/tmp/emulated_tpm/swtpm-sock --log level=20 --tpm2 -d
+
+## QEMU VM
+/usr/local/bin/qemu-system-x86_64 \
+-nodefaults \
 -cpu host,hv-time,hv-relaxed,hv-vapic,hv-spinlocks=0x1fff \
--bios /usr/share/ovmf/OVMF.fd \
--M q35,accel=kvm,kernel-irqchip=split \
+-global driver=cfi.pflash01,property=secure,value=on \
+    -drive if=pflash,format=raw,unit=0,file=OVMF_CODE-need-smm.fd,readonly=on \
+    -drive if=pflash,format=raw,unit=1,file=OVMF_VARS-need-smm.fd \
+-M q35,smm=on,accel=kvm,kernel-irqchip=split \
 -device intel-iommu,intremap=on \
 -rtc base=localtime \
 -smp 4,sockets=1,cores=2,threads=2 \
@@ -10,15 +18,13 @@ qemu-system-x86_64 \
     -object memory-backend-file,id=mem0,mem-path=/dev/hugepages,size=4G -numa node,nodeid=0,memdev=mem0 \
     -object memory-backend-file,id=mem1,mem-path=/dev/hugepages,size=4G -numa node,nodeid=1,memdev=mem1 \
     -mem-prealloc \
+-tpmdev passthrough,id=tpm0,path=/dev/tpm0 \
+-device tpm-tis,tpmdev=tpm0 \
 -device virtio-net-pci,netdev=net0 -netdev user,id=net0,hostfwd=tcp::3389-:3389 \
 -usb -device usb-kbd -device usb-mouse \
 -device ich9-intel-hda -device hda-output \
 -display egl-headless,rendernode=/dev/dri/card0 \
--device qxl-vga,id=video0,ram_size=134217728,vram_size=134217728,vgamem_mb=512 \
--device virtio-serial-pci \
--spice port=9000,disable-ticketing,image-compression=auto_glz,jpeg-wan-compression=always,playback-compression=off,zlib-glz-wan-compression=never,streaming-video=filter,agent-mouse=on \
--device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
--chardev spicevmc,id=spicechannel0,name=vdagent \
--drive file=mijksrv-vm-w10.img,if=virtio,cache.direct=on,aio=native,format=raw \
+-device virtio-gpu-gl-pci \
+-drive file=/home/mike/qemu/windows/hda.img,if=virtio,cache.direct=on,aio=native,format=raw \
 -boot c \
 -monitor stdio
