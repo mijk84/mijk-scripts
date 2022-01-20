df168133f90a1365bba51765d1d970000e6cec95
Mike Ladouceur <mgladouceur@gmail.com>
2022-01-20 09:06:30 -0500

    Delete yt-play.sh
    I don't use this anymore and don't know if this works anymore

--- yt-play.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ $# -eq 0 ]; then
-    echo "No arguments provided"
-    exit 1
-fi
-
-video=$(echo "$1" | sed -r 's/^.{32}//' | cut -f1 -d"&")
-
-youtube-dl --prefer-insecure -o - "$video" -f 17 | mplayer -
