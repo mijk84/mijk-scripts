a74a4bfb892df1291fa59c3a555ea4d48570b204
Mike Ladouceur <mgladouceur@gmail.com>
2023-06-30 14:22:19 -0400

    Update sysinfo.sh
    Just output formatting

--- sysinfo.sh
+++ sysinfo.sh
@@ -6,12 +6,13 @@ n=$(tput sgr0)
 echo This simply copies your system information to the clipboard to paste in a chat room.
 echo
 
-SI="echo ${b}Hostname${n}: $(hostname) \
-    ${b}• OS${n}: $(cat /etc/lsb-release | grep DESCRIPTION | cut -c 22- | head -c -2) \
-    ${b}• CPU${n}: $(cat /proc/cpuinfo | grep cores | tail -n1 | awk '{ print $4 }')x $(cat /proc/cpuinfo | grep model | tail -n1 | awk '{$1=$2=$3=""; print $0}') \
-    ${b}• RAM${n}: $(echo $(grep MemTotal /proc/meminfo | awk '{print $2}') / 1000000 | bc)GB \
-    ${b}• Storage${n}: $(df | grep '^/dev/[hs]d' | awk '{s+=$2} END {print s/1048576}')GB Total ($(df | grep '^/dev/[hs]d' | awk '{s+=$4} END {print s/1048576}')GB Free) \
-    ${b}• VGA${n}: $(lspci | grep VGA | awk '{$1=$2=$3=$4=""; print $0}') ${b}• Uptime${n}: $(uptime --pretty | cut -c 4-)" \
+SI="echo ${b}•• Hostname${n}: $(hostname) \
+    ${b}•• OS${n}: $(cat /etc/lsb-release | grep DESCRIPTION | cut -c 22- | head -c -2) \
+    ${b}•• CPU${n}: $(cat /proc/cpuinfo | grep cores | tail -n1 | awk '{ print $4 }')x $(cat /proc/cpuinfo | grep model | tail -n1 | awk '{$1=$2=$3=""; print $0}') \
+    ${b}•• RAM${n}: $(echo $(grep MemTotal /proc/meminfo | awk '{print $2}') / 1000000 | bc)GB \
+    ${b}•• Storage${n}: $(df | grep '^/dev/[hs]d' | awk '{s+=$2} END {print s/1048576}')GB Total ($(df | grep '^/dev/[hs]d' | awk '{s+=$4} END {print s/1048576}')GB Free) \
+    ${b}•• VGA${n}: $(lspci | grep VGA | awk '{$1=$2=$3=$4=""; print $0}') \
+    ${b}•• Uptime${n}: $(uptime --pretty | cut -c 4-)" \
 
 if ! which xclip > /dev/null; then
     echo
@@ -33,8 +34,8 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 case "$1" in
-    -s) echo $($SI) ;;
-    --shell) echo $($SI) ;;
+    -s) echo $($SI) ${b}••${n} ;;
+    --shell) echo $($SI) ${b}••${n} ;;
     -?) echo Usage: sysinfo && echo      sysinfo -s [This pastes to the shell instead] && echo      system --shell [This pastes to the shell instead] ;;
     --help) echo Usage: sysinfo && echo      sysinfo -s [This pastes to the shell instead] && echo      sysinfo --shell [This pastes to the shell instead] ;;
     *) echo Try running this on its own or run: sysinfo --help ;;
