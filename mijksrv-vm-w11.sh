04aa840840d0d7e36d6525453bf0cbaaa6613615
Mike Ladouceur <mgladouceur@gmail.com>
2023-11-01 15:47:46 -0400

    Update mijksrv-vm-w11.sh
    Just a few changes that I believe ought to increase video performance some.

--- mijksrv-vm-w11.sh
+++ mijksrv-vm-w11.sh
@@ -13,23 +13,24 @@ swtpm socket \
 -nodefaults \
 -cpu host,hv-time,hv-relaxed,hv-vapic,hv-spinlocks=0x1fff \
 -global driver=cfi.pflash01,property=secure,value=on \
-    -drive if=pflash,format=raw,unit=0,file=OVMF_CODE-need-smm.fd,readonly=on \
-    -drive if=pflash,format=raw,unit=1,file=OVMF_VARS-need-smm.fd \
+    -drive if=pflash,format=raw,unit=0,file=OVMF_CODE_4M.secboot.fd,readonly=on \
+    -drive if=pflash,format=raw,unit=1,file=OVMF_VARS_4M.fd \
 -M q35,smm=on,accel=kvm,kernel-irqchip=split \
 -device intel-iommu,intremap=on \
 -rtc base=localtime \
--smp 4,sockets=1,cores=2,threads=2 \
+-smp 8,sockets=1,cores=4,threads=2 \
 -m 8192 \
-    -object memory-backend-file,id=mem0,mem-path=/dev/hugepages,size=4G -numa node,nodeid=0,memdev=mem0 \
-    -object memory-backend-file,id=mem1,mem-path=/dev/hugepages,size=4G -numa node,nodeid=1,memdev=mem1 \
     -mem-prealloc \
--tpmdev passthrough,id=tpm0,path=/dev/tpm0 \
+-tpmdev passthrough,id=tpm0,path=/dev/tpm0,cancel-path=/tmp/foo-cancel \
 -device tpm-tis,tpmdev=tpm0 \
 -device virtio-net-pci,netdev=net0 -netdev user,id=net0,hostfwd=tcp::3389-:3389 \
--usb -device usb-kbd -device usb-mouse \
--device ich9-intel-hda -device hda-output \
--display egl-headless,rendernode=/dev/dri/card0 \
--device virtio-gpu-gl-pci \
--drive file=/home/mike/qemu/windows/hda.img,if=virtio,cache.direct=on,aio=native,format=raw \
+-device nec-usb-xhci,id=xhci \
+-device usb-kbd,bus=xhci.0 -device usb-tablet,bus=xhci.0 \
+-display egl-headless,rendernode=/dev/dri/card0,gl=on \
+-device qxl-vga,id=video0,ram_size=134217728,vram_size=134217728,vgamem_mb=512 \
+-spice disable-ticketing=on,image-compression=off,seamless-migration=on,unix=on,addr=/run/user/1000/spice.sock,gl=on \
+-device virtio-serial -chardev spicevmc,id=vdagent,debug=0,name=vdagent \
+-device virtserialport,chardev=vdagent,name=com.redhat.spice.0 \
+-drive file=hda.img,if=virtio,cache.direct=on,aio=native,format=qcow2 \
 -boot c \
 -monitor stdio
