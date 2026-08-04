dbc0776978c16eb6c8f1b5f9bbca6b47fec84fff
Mike Ladouceur <mgladouceur@gmail.com>
2026-08-04 14:11:07 -0400

    Add QEMU VM startup script for configuration
    Effectively the same as the Windows scripts with "@echo off" replaced with "#!/bin/sh" and all carets (^), replaced with backslashes (\).

---
+++ mijk-vm-w2k.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+qemu-system-x86_64 \
+-nodefaults \
+-M pc,hmat=on \
+--accel tcg,tb-size=512 \
+-cpu pentium3,hv-time,hv-relaxed,hv-vapic,hv-spinlocks=0x1fff \
+        -smp 1,cores=1,threads=1,sockets=1,maxcpus=1 \
+-rtc base=localtime \
+-m 3072,slots=3,maxmem=4G \
+        -mem-prealloc \
+        -object memory-backend-ram,size=1G,id=m0 \
+        -object memory-backend-ram,size=1G,id=m1 \
+        -object memory-backend-ram,size=1G,id=m2 \
+        -numa node,nodeid=0,memdev=m0,cpus=0 \
+        -numa node,nodeid=1,memdev=m1 \
+        -numa node,nodeid=2,memdev=m2,initiator=0 \
+-display egl-headless \
+-spice disable-ticketing=on,unix=off,addr=0.0.0.0,port=5900,video-codec=gstreamer:h264 \
+-device vmware-svga,id=video0,vgamem_mb=32 \
+-device pcnet,netdev=net0 -netdev user,id=net0 \
+-device ich9-usb-uhci1,id=uhci \
+-device usb-kbd,bus=uhci.0 -device usb-tablet,bus=uhci.0 \
+-device AC97 -audio spice \
+-drive file=hda.img,format=raw,media=disk \
+-boot c \
+-monitor stdio
